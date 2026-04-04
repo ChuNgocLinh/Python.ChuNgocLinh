76df231e6c7ad1084729472cc0e9e6b5d71aa030
ChuNgocLinh <chubake111933@gmail.com>
2026-04-04 07:48:48 +0700

    Add files via upload

---
+++ Chương 5/baithuchanh1.py
@@ -0,0 +1,15 @@
+# Sinh viên: Phạm Gia Khánh - MSSV: 20230797 - Lớp: DCCNTT14.2
+
+ten_file = "du_lieu.txt"
+n = int(input("Nhập số dòng n cần đọc: "))
+
+try:
+    with open(ten_file, 'r', encoding='utf-8') as file:
+        print(f"\n--- {n} dòng đầu tiên của file ---")
+        for i in range(n):
+            dong = file.readline()
+            if not dong:
+                break
+            print(dong, end="") 
+except FileNotFoundError:
+    print(f"Lỗi: Không tìm thấy file '{ten_file}' trong thư mục!")
